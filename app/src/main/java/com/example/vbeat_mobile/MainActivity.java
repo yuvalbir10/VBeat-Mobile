7f2bc16d845bc5ad4c31427b9a7a13295424886e
yuvalbir10 <yuvalbir10@gmail.com>
2019-08-02 13:16:45 +0300

    add sign in fragment

--- app/src/main/java/com/example/vbeat_mobile/MainActivity.java
+++ app/src/main/java/com/example/vbeat_mobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.vbeat_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogInFragment logInFragment = new LogInFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.main_container, logInFragment);
+        transaction.addToBackStack("TAG");
+        transaction.commit();
     }
 }
