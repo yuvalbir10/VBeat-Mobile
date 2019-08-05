adaba1af6327d14f7e295c0da6c989814f279538
GI <guyishay2@gmail.com>
2019-08-05 19:06:00 +0300

    getting path for image file

--- app/src/main/java/com/example/vbeat_mobile/UI/UploadPostFragment.java
+++ app/src/main/java/com/example/vbeat_mobile/UI/UploadPostFragment.java
@@ -1,13 +1,20 @@
 package com.example.vbeat_mobile.UI;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +31,7 @@ import static android.app.Activity.RESULT_OK;
  */
 public class UploadPostFragment extends Fragment {
     private static final int PICK_IMAGE_REQUEST = 1, PICK_MUSIC_REQUEST = 2;
+    private static final String TAG = "UploadPostFragment";
 
     private ImageView imageView;
     private Button chooseImageButton, chooseMusicButton;
@@ -75,17 +83,65 @@ public class UploadPostFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        
+
+        // can't continue if data is null
+        // or if we have no context
+        if(data == null || getContext() == null) {
+            Log.e(TAG, "data || getContext() were null");
+            return;
+        }
 
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK){
             imageUri = data.getData();
             imageView.setVisibility(View.VISIBLE);
 
+            String path = getRealPathFromURI_API19(getContext(), imageUri);
 
+            // if we're getting a good path
+            // load into image view
+            if(path != null) {
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+
+                View v = getView();
+                if(v == null){
+                    return;
+                }
+
+                ImageView image = v.findViewById(R.id.imageView);
+                image.setImageBitmap(bitmap);
+            }
         }
 
         if(requestCode == PICK_MUSIC_REQUEST && resultCode == RESULT_OK){
             musicUri = data.getData();
         }
+    }
+
+    private static String getRealPathFromURI_API19(Context context, Uri uri){
+        String filePath = "";
+        String wholeID = DocumentsContract.getDocumentId(uri);
+
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = { MediaStore.Images.Media.DATA };
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                column, sel, new String[]{ id }, null);
+
+        if(cursor == null){
+            return null;
+        }
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
     }
 }
