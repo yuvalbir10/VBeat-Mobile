5ab6f6d10ee6a6a40669295324c59f6cc798344f
GI <guyishay2@gmail.com>
2019-08-10 13:22:13 +0300

    initializing app local db singleton instance

--- app/src/main/java/com/example/vbeat_mobile/UI/MainActivity.java
+++ app/src/main/java/com/example/vbeat_mobile/UI/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.vbeat_mobile.UI;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
+import android.app.Application;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.example.vbeat_mobile.R;
+import com.example.vbeat_mobile.backend.cache.AppLocalDB;
 import com.example.vbeat_mobile.backend.cache.FirebaseImageCache;
 import com.example.vbeat_mobile.backend.user.FirebaseUserManager;
 import com.example.vbeat_mobile.backend.user.UserManager;
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void initialize(){
         // initialize firebase image cache
-        FirebaseImageCache.setApplicationContext(getApplication());
+        Application appContext = getApplication();
+        FirebaseImageCache.setApplicationContext(appContext);
+        AppLocalDB.initialize(appContext);
     }
 }
