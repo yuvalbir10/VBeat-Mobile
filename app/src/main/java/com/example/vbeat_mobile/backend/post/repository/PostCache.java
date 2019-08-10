f072c645dad02ab89d87012de93df1d02e8e762e
GI <guyishay2@gmail.com>
2019-08-10 13:58:53 +0300

    implementing post cache

--- app/src/main/java/com/example/vbeat_mobile/backend/post/repository/PostCache.java
+++ app/src/main/java/com/example/vbeat_mobile/backend/post/repository/PostCache.java
@@ -3,14 +3,23 @@ package com.example.vbeat_mobile.backend.post.repository;
 import com.example.vbeat_mobile.backend.cache.AppLocalDB;
 import com.example.vbeat_mobile.backend.post.VBeatPostModel;
 
+import java.util.List;
+
 public class PostCache {
     // cache is empty and not implemented for now
     public VBeatPostModel getPost(String postId){
-        return null;
+        List<VBeatPostModel> postList = AppLocalDB.getInstance().db.postDao().getPost(postId);
+
+        if(postList.size() == 0) {
+            return null;
+        } else {
+            return postList.get(0);
+        }
+
     }
 
     // cache is currently not implemented
     public void savePost(VBeatPostModel post) {
-        return;
+        AppLocalDB.getInstance().db.postDao().insertAll(post);
     }
 }
