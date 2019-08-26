1b340ba6780e311a587c49917d4ba13e40f42efe
GI <guyishay2@gmail.com>
2019-08-26 15:52:34 +0300

    write comment manager

--- app/src/main/java/com/example/vbeat_mobile/backend/comment/CommentModel.java
+++ app/src/main/java/com/example/vbeat_mobile/backend/comment/CommentModel.java
@@ -1,18 +1,25 @@
 package com.example.vbeat_mobile.backend.comment;
 
 public class CommentModel {
+    private String commentId;
     private String userId;
     private String commentText;
     private String postId;
 
     public CommentModel(
+            String commentId,
             String userId,
             String commentText,
             String postId
     ){
+        this.commentId = commentId;
         this.userId = userId;
         this.commentText = commentText;
         this.postId = postId;
+    }
+
+    public String getCommentId() {
+        return commentId;
     }
 
     public String getUserId() {
---
+++ app/src/main/java/com/example/vbeat_mobile/backend/comment/FirebaseCommentManager.java
@@ -0,0 +1,95 @@
+package com.example.vbeat_mobile.backend.comment;
+
+import android.util.Log;
+
+import com.example.vbeat_mobile.backend.user.FirebaseUserManager;
+import com.example.vbeat_mobile.backend.user.VBeatUserModel;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class FirebaseCommentManager {
+    private static final String TAG = "FirebaseCommentManager";
+    private static final String COMMENT_COLLECTION = "comments";
+
+    private static class FirebaseCommentManagerInstanceHolder {
+        private static FirebaseCommentManager instance = new FirebaseCommentManager();
+    }
+
+    public static FirebaseCommentManager getInstance() {
+        return FirebaseCommentManagerInstanceHolder.instance;
+    }
+
+    private FirebaseCommentManager() {
+        Log.d(TAG, "FirebaseCommentManager instance created");
+    }
+
+    public CommentModel comment(String commentText, String postId) throws CommentException {
+        FirebaseFirestore instance = FirebaseFirestore.getInstance();
+        FirebaseUserManager firebaseUserManager = FirebaseUserManager.getInstance();
+        VBeatUserModel currentUser = firebaseUserManager.getCurrentUser();
+
+        if (currentUser == null) {
+            Log.w(TAG, "currentUser == null on FirebaseCommentManager");
+            throw new CommentException("unable to post comment because user is not logged in");
+        }
+
+        try {
+            DocumentReference docRef = Tasks.await(instance.collection(COMMENT_COLLECTION).add(
+                    createFirebaseCommentFromParams(
+                            commentText, postId, currentUser.getUserId()
+                    )));
+
+            return new CommentModel(docRef.getId(), currentUser.getUserId(), commentText, postId);
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "Unable to add comment", e);
+            throw new CommentException(e.getMessage());
+        }
+    }
+
+    public void deleteComment(String commentId) throws CommentException {
+        FirebaseFirestore instance = FirebaseFirestore.getInstance();
+
+        try {
+            Void v = Tasks.await(
+                    instance.collection(COMMENT_COLLECTION).document(commentId).delete()
+            );
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "unable to delete comment", e);
+            throw new CommentException(e.getMessage());
+        }
+
+    }
+
+    // this method assumes the comment exists
+    public void editComment(String commentId, String commentText) throws CommentException {
+        FirebaseFirestore instance = FirebaseFirestore.getInstance();
+
+        try {
+            Tasks.await(
+                    instance.collection(COMMENT_COLLECTION).document(commentId)
+                            .update(
+                                    "commentText", commentText
+                            )
+            );
+        } catch(ExecutionException | InterruptedException e) {
+            Log.e(TAG, "unable to edit comment", e);
+            throw new CommentException(e.getMessage());
+        }
+
+    }
+
+    private Map<String, Object> createFirebaseCommentFromParams(String commentText,
+                                                                String postId,
+                                                                String userId) {
+        Map<String, Object> obj = new HashMap<>();
+        obj.put("commentText", commentText);
+        obj.put("postId", postId);
+        obj.put("userId", userId);
+        return obj;
+    }
+}
