a7b781c3cc4dac0d0097383544d41e196e6b0174
GI <guyishay2@gmail.com>
2019-08-28 11:37:29 +0300

    implementing getUser & getUsers

--- app/src/main/java/com/example/vbeat_mobile/backend/user/FirebaseUserManager.java
+++ app/src/main/java/com/example/vbeat_mobile/backend/user/FirebaseUserManager.java
@@ -7,17 +7,21 @@ import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 /*
-* This can be a singleton
-* But I don't think it makes much difference if it isn't
-* */
+ * This can be a singleton
+ * But I don't think it makes much difference if it isn't
+ * */
 public class FirebaseUserManager implements UserManager {
     private static final String TAG = "FirebaseUserManager";
     private static final String USERS_COLLECTION = "vbeat_users";
@@ -28,11 +32,11 @@ public class FirebaseUserManager implements UserManager {
         private static FirebaseUserManager instance = new FirebaseUserManager();
     }
 
-    public static FirebaseUserManager getInstance(){
+    public static FirebaseUserManager getInstance() {
         return FirebaseUserManagerInstanceHolder.instance;
     }
 
-    private FirebaseUserManager(){
+    private FirebaseUserManager() {
         mAuth = FirebaseAuth.getInstance();
     }
 
@@ -44,7 +48,7 @@ public class FirebaseUserManager implements UserManager {
 
     @Override
     public void createAccount(String email, String password) throws UserRegistrationFailedException {
-        if(isUserLoggedIn()) {
+        if (isUserLoggedIn()) {
             throw new IllegalStateException("can't create account while user is logged in");
         }
 
@@ -57,18 +61,17 @@ public class FirebaseUserManager implements UserManager {
             // register user with firebase authentication
             Tasks.await(t);
 
-            if(t.getResult() == null) {
-                Log.wtf(TAG, "await failed (?)");
-                throw new ExecutionException(new RuntimeException("Tasks.await failed"));
-            }
+            verifyResult(t);
 
             VBeatUserModel userToBeRegistered = new VBeatUserModel(
                     email, getDisplayNameFromEmail(email),
-                    t.getResult().getUser().getUid()
+                    Objects.requireNonNull(t.getResult()).getUser().getUid()
             );
             // write user details into users collection
-            instance.collection(USERS_COLLECTION).document(userToBeRegistered.getUserId())
-                    .update(createFirebaseFromModel(userToBeRegistered));
+            Tasks.await(
+                    instance.collection(USERS_COLLECTION).document(userToBeRegistered.getUserId())
+                            .update(createFirebaseFromModel(userToBeRegistered))
+            );
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
             Log.d(TAG, "register user has failed with exception", e);
@@ -76,8 +79,8 @@ public class FirebaseUserManager implements UserManager {
         // get exception if there was one
         Exception e = t.getException();
 
-        if(!t.isSuccessful()) {
-            if(e != null){
+        if (!t.isSuccessful()) {
+            if (e != null) {
                 throw new UserRegistrationFailedException(e.getMessage());
             } else {
                 throw new UserRegistrationFailedException("unknown reason");
@@ -89,7 +92,7 @@ public class FirebaseUserManager implements UserManager {
     public boolean deleteUser() {
         FirebaseUser user = mAuth.getCurrentUser();
 
-        if(user == null) {
+        if (user == null) {
             throw new IllegalStateException("you have to login to delete");
         }
 
@@ -114,7 +117,7 @@ public class FirebaseUserManager implements UserManager {
 
     @Override
     public void login(String email, String password) throws UserLoginFailedException {
-        if(isUserLoggedIn()) {
+        if (isUserLoggedIn()) {
             throw new UserLoginFailedException("can't login while user is already logged in");
         }
 
@@ -130,7 +133,7 @@ public class FirebaseUserManager implements UserManager {
         }
 
         // check if we managed to login or not
-        if(!t.isSuccessful()) {
+        if (!t.isSuccessful()) {
             Exception e = t.getException();
             if (e != null) {
                 throw new UserLoginFailedException(e.getMessage());
@@ -140,15 +143,53 @@ public class FirebaseUserManager implements UserManager {
         }
     }
 
-    public List<VBeatUserModel> getUsers(List<String> userIds) {
-        throw new RuntimeException("not implemented");
+    public List<VBeatUserModel> getUsers(List<String> userIds) throws UserBackendException {
+        FirebaseFirestore instance = FirebaseFirestore.getInstance();
+        List<VBeatUserModel> userModels = new LinkedList<>();
+
+        Task<QuerySnapshot> t = instance.collection(USERS_COLLECTION).get();
+
+        try {
+            verifyResult(t);
+            Tasks.await(t);
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "getUsers failed with exception", e);
+            throw new UserBackendException(e.getMessage());
+        }
+
+        List<DocumentSnapshot> dsList = Objects.requireNonNull(t.getResult()).getDocuments();
+
+        for (DocumentSnapshot ds : dsList) {
+            VBeatUserModel userModel = getModelFromFirebase(ds);
+            if (userIds.contains(userModel.getUserId())) {
+                userModels.add(userModel);
+            }
+        }
+
+        return userModels;
     }
 
-    public VBeatUserModel getUser(String userId){
-        throw new RuntimeException("method not implemented");
+    public VBeatUserModel getUser(String userId) throws UserBackendException {
+        FirebaseFirestore instance = FirebaseFirestore.getInstance();
+
+        Task<DocumentSnapshot> t = instance.collection(USERS_COLLECTION).document(userId).get();
+        try {
+            Tasks.await(t);
+            verifyResult(t);
+        } catch (ExecutionException | InterruptedException e) {
+            Log.e(TAG, "getUser failed with exception", e);
+            throw new UserBackendException(e.getMessage());
+        }
+        
+        DocumentSnapshot ds = t.getResult();
+        if(ds == null) {
+            throw new UserBackendException("unable to find user");
+        }
+
+        return getModelFromFirebase(ds);
     }
 
-    private static Map<String, Object> createFirebaseFromModel(VBeatUserModel vBeatUserModel){
+    private static Map<String, Object> createFirebaseFromModel(VBeatUserModel vBeatUserModel) {
         Map<String, Object> obj = new HashMap<>();
         obj.put("display_name", vBeatUserModel.getDisplayName());
         obj.put("email", vBeatUserModel.getEmail());
@@ -156,7 +197,22 @@ public class FirebaseUserManager implements UserManager {
         return obj;
     }
 
+    private static VBeatUserModel getModelFromFirebase(DocumentSnapshot ds) {
+        return new VBeatUserModel(
+                (String) ds.get("email"),
+                (String) ds.get("display_name"),
+                ds.getId()
+        );
+    }
+
     private static String getDisplayNameFromEmail(String email) {
         return email.substring(0, email.indexOf('@'));
+    }
+
+    private static void verifyResult(Task t) throws ExecutionException {
+        if (t.getResult() == null) {
+            Log.wtf(TAG, "verifyResult failed");
+            throw new ExecutionException(new RuntimeException("failed verify result check"));
+        }
     }
 }
