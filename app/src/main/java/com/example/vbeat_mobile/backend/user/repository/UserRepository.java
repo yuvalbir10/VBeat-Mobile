40e3d82d4ace367f57140e086f62b97c3cef2bc5
GI <guyishay2@gmail.com>
2019-08-30 15:46:19 +0300

    creating cache mechanism for get users method

--- app/src/main/java/com/example/vbeat_mobile/backend/user/repository/UserRepository.java
+++ app/src/main/java/com/example/vbeat_mobile/backend/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.vbeat_mobile.backend.user.repository;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
@@ -8,11 +10,19 @@ import com.example.vbeat_mobile.backend.user.UserBackendException;
 import com.example.vbeat_mobile.backend.user.VBeatUserModel;
 import com.example.vbeat_mobile.viewmodel.UserViewModel;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class UserRepository {
     private UserCache userCache = null;
+    private static final String TAG = "UserRepository";
 
     private static class UserRepositoryInstanceHolder {
         private static UserRepository instance = new UserRepository();
+    }
+
+    public static UserRepository getInstance() {
+        return UserRepositoryInstanceHolder.instance;
     }
 
     private UserRepository(){
@@ -39,6 +49,66 @@ public class UserRepository {
         }).start();
 
         return userViewModel;
+    }
+
+    public LiveData<List<VBeatUserModel>> getUsers(final List<String> userIds) {
+        final MutableLiveData<List<VBeatUserModel>> userModelList = new MutableLiveData<>();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // get all users that we can from cache
+                List<VBeatUserModel> fetchedList = tryToLoadUsersFromCache(userIds);
+                List<String> remainingUserIds = removeFetchedIds(fetchedList, userIds);
+
+                // get remaining users and add them to the list
+                if(remainingUserIds.size() > 0){
+                    boolean isSuccessful =
+                            fetchRemainingUserIdsFromFirebase(fetchedList, remainingUserIds, userModelList);
+                    // if we haven't managed to get user list
+                    // fail the entire method
+                    if(!isSuccessful) {
+                        return;
+                    }
+                }
+
+               userModelList.postValue(fetchedList);
+            }
+        }).start();
+
+        return userModelList;
+    }
+
+    private boolean fetchRemainingUserIdsFromFirebase(List<VBeatUserModel> fetchedList, List<String> remainingUserIds, MutableLiveData<List<VBeatUserModel>> userModelList) {
+        try {
+            fetchedList.addAll(
+                    FirebaseUserManager.getInstance().getUsers(remainingUserIds)
+            );
+        } catch (UserBackendException e) {
+            Log.e(TAG, "getUsers failed with exception", e);
+            // set null unless we were able to get all users
+            userModelList.postValue(null);
+        }
+    }
+
+    private List<String> removeFetchedIds(List<VBeatUserModel> fetchedList, List<String> userIds) {
+        for(VBeatUserModel model : fetchedList) {
+            userIds.remove(model.getUserId());
+        }
+
+        return userIds;
+    }
+
+    private List<VBeatUserModel> tryToLoadUsersFromCache(List<String> userIds) {
+        List<VBeatUserModel> cacheList = new LinkedList<>();
+        for(String userId : userIds) {
+            VBeatUserModel userModel = userCache.get(userId);
+            if(userModel != null) {
+                cacheList.add(userModel);
+            }
+        }
+
+        return cacheList;
     }
 
     private UserViewModel getViewModelFromModel(VBeatUserModel userModel) {
