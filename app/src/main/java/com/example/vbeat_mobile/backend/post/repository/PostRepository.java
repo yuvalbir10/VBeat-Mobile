0444c751cc40a1ea97e063b331d555e810a1b563
GI <guyishay2@gmail.com>
2019-08-31 20:17:51 +0300

    update post cache

--- app/src/main/java/com/example/vbeat_mobile/backend/post/repository/PostRepository.java
+++ app/src/main/java/com/example/vbeat_mobile/backend/post/repository/PostRepository.java
@@ -59,6 +59,11 @@ public class PostRepository {
     public boolean editPost(String postId, String description) {
         try {
             FirebasePostManager.getInstance().editPost(postId, description);
+
+            // update post cache
+            VBeatPostModel postModel = postCache.getPost(postId);
+            postModel.setDescription(description);
+            postCache.savePost(postModel);
             return true;
         } catch (UploadPostFailedException e) {
             Log.e(TAG, "unable to edit post", e);
