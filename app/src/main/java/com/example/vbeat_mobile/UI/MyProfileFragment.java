5836fdf762ee08a426638478359e8229527f7496
yuvalbir10 <yuvalbir10@gmail.com>
2019-08-31 15:41:00 +0300

    add profileFragment to navigation

--- app/src/main/java/com/example/vbeat_mobile/UI/MyProfileFragment.java
+++ app/src/main/java/com/example/vbeat_mobile/UI/MyProfileFragment.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.vbeat_mobile.R;
 import com.example.vbeat_mobile.backend.post.repository.PostRepository;
@@ -32,6 +33,7 @@ public class MyProfileFragment extends Fragment {
     private RecyclerView postsRecyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private FeedRecyclerViewAdapter feedAdapter;
+    private TextView usernameTextView;
 
     public MyProfileFragment() {
         // Required empty public constructor
@@ -44,6 +46,8 @@ public class MyProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_my_profile, container, false);
         postsRecyclerView = v.findViewById(R.id.posts_RecyclerView);
+        usernameTextView = v.findViewById(R.id.username_textView);
+        usernameTextView.setText(FirebaseUserManager.getInstance().getCurrentUser().getDisplayName());
         postsRecyclerView.setHasFixedSize(true);
 
         layoutManager = new LinearLayoutManager(this.getContext());
